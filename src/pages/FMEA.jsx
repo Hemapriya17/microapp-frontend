cd4c7d1d67e131cd099e7a60677055b9ba465f01
Hemapriya17 <hema.learny@gmail.com>
2024-01-03 09:48:23 +0530

    Updated frontend

---
+++ src/pages/FMEA.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { indigo } from "@mui/material/colors";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import { saveAs } from "file-saver";
+import axios from "axios";
+
+const FMEA = () => {
+  const [fmeaName, setFmeaName] = useState("");
+  const [fmeaTable, setFmeaTable] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleDownload = () => {
+    const blob = new Blob([fmeaTable], {
+      type: "text/plain;charset=utf-8",
+    });
+    saveAs(blob, "fmeaTable.txt");
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+  
+    try {
+      setLoading(true);
+  
+      // Generate FMEA
+      const fmeaResponse = await axios.post(
+        "http://3.19.219.191/generate-fmea",
+        { fmeaName }
+      );
+  
+      const data = fmeaResponse.data;
+  
+      if (fmeaResponse.status !== 200) {
+        throw new Error(`Request failed with status ${fmeaResponse.status}`);
+      }
+  
+      setFmeaTable(data.fmeaTable);
+    } catch (error) {
+      console.error("Error generating FMEA:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(indigo[900]),
+    backgroundColor: indigo[900],
+    "&:hover": {
+      backgroundColor: indigo[900],
+    },
+  }));
+
+  return (
+    <div>
+      <h3>FMEA Generator</h3>
+      <br />
+      <br />
+      <form onSubmit={onSubmit}>
+        <TextField
+          id="outlined-basic"
+          sx={{
+            width: 500,
+            maxWidth: "100%",
+          }}
+          fullWidth
+          type="text"
+          variant="outlined"
+          size="small"
+          value={fmeaName}
+          onChange={(e) => setFmeaName(e.target.value)}
+        />
+        <br />
+        <br />
+        <Stack spacing={37} direction="row">
+          <ColorButton type="submit" variant="contained">
+            Submit
+          </ColorButton>
+          <ColorButton variant="contained" onClick={handleDownload}>
+            Download
+          </ColorButton>
+        </Stack>
+      </form>
+      <br />
+      <br />
+      {loading && <p>Loading...</p>}
+      <div>
+        <pre>{fmeaTable}</pre>
+      </div>
+    </div>
+  );
+};
+
+export default FMEA;
---
+++ src/pages/ProcessMap.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { indigo } from "@mui/material/colors";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import mermaid from "mermaid";
+
+const ProcessMap = () => {
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [mermaidCode, setMermaidCode] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      setLoading(true);
+
+      // Generate Mermaid code
+      const mermaidResponse = await axios.post(
+        "http://127.0.0.1:5000/process-map",
+        { name }
+      );
+      const generatedMermaidCode = mermaidResponse.data.mermaidImage;
+
+      setMermaidCode(generatedMermaidCode);
+
+      // Render the sequence diagram using mermaid
+      mermaid.initialize({ startOnLoad: true });
+      mermaid.render("graph", generatedMermaidCode, (svgCode) => {
+        // Inject the SVG code into the DOM
+        document.getElementById("sequence-diagram").innerHTML = svgCode;
+      });
+    } catch (error) {
+      console.error("Error getting mermaid code:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const ColorButton = styled(Button)(({ theme }) => ({
+    color: theme.palette.getContrastText(indigo[900]),
+    backgroundColor: indigo[900],
+    "&:hover": {
+      backgroundColor: indigo[900],
+    },
+  }));
+  return (
+    <div>
+      <h3>Process Map</h3>
+      <br />
+      <br />
+      <p>
+        Please mention with both action and object(Eg Cleaning a coffee maker,
+        making coffee using coffee maker). For objects like washing machine
+        please mention it by adding a "-" like washing-machine
+      </p>
+      <br />
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="outlined-basic"
+          fullWidth
+          type="text"
+          sx={{
+            width: 500,
+            maxWidth: "100%",
+          }}
+          // label="Enter FMEA Name"
+          variant="outlined"
+          size="small"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <br />
+        <ColorButton type="submit" variant="contained">
+          Submit
+        </ColorButton>
+      </form>
+      <br />
+      <br />
+      {loading && <p>Loading...</p>}
+      <br />
+      {mermaidCode && (
+        <>
+          <h4>Generated Process map:</h4>
+          <br />
+          <img
+            src={`data:image/png;base64,${mermaidCode}`}
+            alt="Generated Diagram"
+          />
+        </>
+      )}
+      <div id="sequence-diagram"></div>
+    </div>
+  );
+};
+
+export default ProcessMap;
